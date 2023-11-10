38532e7d6f466a0cf31e7e114617a13420816686
Ryuzy <ryuzy@kakao.com>
2023-11-10 10:06:12 +0900

    Socket.io 적용

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 import express from "express";
 import morgan from "morgan";
+import cors from 'cors';
 import tweetsRouter from './router/tweets.js';
 import authRouter from './router/auth.js';
 // import dotenv from 'dotenv';
 import { config } from "./config.js";
 // dotenv.config();
+import { initSocket } from "./connection/socket.js";
 
 console.log(process.env.JWT_SECRET);
 const app = express();
 
 app.use(express.json());
 app.use(morgan("dev"));
-
+app.use(cors());
 app.use('/tweets', tweetsRouter);
 app.use('/auth', authRouter);
 
@@ -19,4 +21,5 @@ app.use((req, res, next) => {
     res.sendStatus(404);
 });
 
-app.listen(config.host.port);
+const server = app.listen(config.host.port);
+initSocket(server);
