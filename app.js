7f31f918bf3ebee8c8fff58bf329b906c6dff9f4
Ryuzy <ryuzy@kakao.com>
2023-11-16 14:01:46 +0900

    Mongodb 적용

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ import authRouter from './router/auth.js';
 import { config } from "./config.js";
 // dotenv.config();
 import { initSocket } from "./connection/socket.js";
-import { db } from './db/database.js';
+import { connectDB } from './db/database.js';
 
 console.log(process.env.JWT_SECRET);
 const app = express();
@@ -22,6 +22,7 @@ app.use((req, res, next) => {
     res.sendStatus(404);
 });
 
-// db.getConnection().then(connection => console.log(connection));
-const server = app.listen(config.host.port);
-initSocket(server);
+connectDB().then((db) => {
+    const server = app.listen(config.host.port);
+    initSocket(server);
+}).catch(console.error);
